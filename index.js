6d460a9fc123ca8dd93c63bed0c1ca4e7a5bd3cc
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 18:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var cartItem = {
     itemName: item,
-    itemPrice: Math.floor(Math.random)
+    itemPrice: Math.floor(Math.random())
   }
   cart.push(cartItem)
 }
