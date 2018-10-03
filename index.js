991284ccd2be98de5db840922ef68b73dd1d27d8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 19:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,5 +59,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+// NOTE: FIX!!!
   if (cardNumber===null) { console.log("Sorry, we don't have a credit card on file for you.");}
+
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+
 }
