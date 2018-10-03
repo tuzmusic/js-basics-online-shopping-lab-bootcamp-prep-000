4a2022fc9460b2a1fc868f93ba02878490a920a2
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-03 19:17:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  if !(cart.indexOf(item)) { return 'That item is not in your cart.'}
+  if (cart.indexOf(item)) { return 'That item is not in your cart.'}
 }
 
 function placeOrder(cardNumber) {
